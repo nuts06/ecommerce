9a795fcfa55521deae0a629be36383642393863c
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-19 20:29:41 +0530

    changes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1 className='underline'>Natasha Chandrawat</h1>
+      <Header />
     </>
   )
 }
