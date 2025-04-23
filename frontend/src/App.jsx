4555ee9390d8d5ca440f354592f358da38c54b57
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-23 11:02:40 +0530

    changes in app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={"/"} exact={true} element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
